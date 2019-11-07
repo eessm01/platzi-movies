682d173bc4473f830d5da6cb955c4c2d6f215db9
eessm01 <eessm01@gmail.com>
2019-11-07 14:06:18 -0600

    Basic strategy from passport.js

---
+++ utils/auth/strategies/basic.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/boom'); // permite el manejor de errores
+const bcrypt = require('bcrypt'); // nos ayudara a verificar el password comparado con el de la BD
+
+const UsersService = require('../../../services/users');
+
+// implementando la estrategia
+passport.use(new BasicStrategy(async function(email, password, cb) {
+  const userService = new UsersService();
+
+  try {
+    // search user although the service
+    const user = await userService.getUser({ email });
+
+    if(!user) {
+      return cb(boom.unauthorized(), false);
+    }
+
+    // compare the password from strategy vs password from database
+    if(!(await bcrypt.compare(password, user.password))) {
+      return cb(boom.unauthorized(), false);
+    }
+
+    delete user.password;
+
+    return cb(null, user);
+
+  } catch(error) {
+    return cb(error);
+  }
+}))
